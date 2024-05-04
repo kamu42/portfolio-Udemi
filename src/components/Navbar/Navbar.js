2aaaf66b72cc38dfb9c1972f257ad2f39eba4a51
Camilo Corredor <camilo.nm16@gmail.com>
2024-05-04 13:01:40 -0500

    feat: adding mobile menu and burgguer

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import * as S from "./Navbar.styles"
 
 const Navbar = () => {
+
+    const [showMenuBurguer, setShowMenuBurguer] = useState(false);
+
   return (
     <S.ContainerNavbar>
-        primer container
+        <S.ContainerLogo>
+            <S.ImageLogo src='/public/img/logo.png' alt='Logo Rafa'/>
+            <div>
+                <h1>Camilo Corredor</h1>
+                <h2>Frontend Developer</h2>
+            </div>
+        </S.ContainerLogo>
+
+        <S.ContainerBurguer onClick={()=>setShowMenuBurguer(!showMenuBurguer)}>
+            <S.BurguerMenu/>
+            <S.BurguerMenu/>
+            <S.BurguerMenu/>
+        </S.ContainerBurguer>
+
+        <S.ContainerItems showMenuBurguer = {showMenuBurguer}>
+            <p>Aout Me  </p>
+            <p>  Projects</p>
+            <S.Button>Contact me</S.Button>
+        </S.ContainerItems>
     </S.ContainerNavbar>
   )
 }
